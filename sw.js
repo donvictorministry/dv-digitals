4378c7601a63a4cc7c9edab9865b30e17850fab7
donvictorministry <donvictorministry@gmail.com>
2026-09-20 19:53:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,46 @@
 
+const CACHE_NAME = 'dv-icons-images-v2';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CORE_ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Only intercept requests for your own app's files (ignore external APIs/CDNs)
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Return the cached file if we have it, otherwise fetch from the network
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
